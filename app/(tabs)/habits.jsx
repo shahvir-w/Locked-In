990bdd2ffc231a7baf4a653cf35f7fd434165d72
shahvir-w <shahvir05@icloud.com>
2024-08-20 13:11:32 -0400

    Made changes to home page

--- app/(tabs)/habits.jsx
+++ app/(tabs)/habits.jsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function habits() {
   return (
-    <View>
-      <Text>habits</Text>
-    </View>
+    <SafeAreaView style={[styles.main]}>
+        <View style={styles.container}>
+          <Image source={require('../../assets/images/Locked-In-Logo.png')}
+            style={{
+                height: 60,
+                width: 55,
+                left: 0,
+            }}
+          />
+          <View>
+            <Text style={{
+                fontFamily: 'JockeyOne',
+                fontSize: 25,
+                color: '#7C81FC',
+                top:13,
+                left: -12,
+            }}>locked in</Text>
+          </View>
+        </View>
+
+      </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: Colors.BLACK,
+  },
+  container: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    top: -360,
+    left: -115,
+  }
+});
--- constants/icons.tsx
+++ constants/icons.tsx
@@ -4,12 +4,12 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 
 export const TabIcons = {
     habits: (props: any) => (
-        <FontAwesome6 name="list-check" size={24} {...props} />
+        <FontAwesome6 name="list-check" size={18} {...props} />
     ),
     stats: (props: any) => (
-        <Ionicons name="stats-chart-sharp" size={24} {...props} />
+        <Ionicons name="stats-chart-sharp" size={18} {...props} />
     ),
     info: (props: any) => (
-        <AntDesign name="infocirlceo" size={24} {...props} />
+        <AntDesign name="infocirlceo" size={18} {...props} />
     ),
 };
